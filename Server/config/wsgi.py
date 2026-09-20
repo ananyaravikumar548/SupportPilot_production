5036c8db5ce96f9b00df96f8d3c794fc592a98a9
ananya <ananyaravikumar71@gmail.com>
2026-09-20 19:37:19 +0530

    perf: limit thread memory and enforce lazy model loading

--- Server/config/wsgi.py
+++ Server/config/wsgi.py
@@ -9,6 +9,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
